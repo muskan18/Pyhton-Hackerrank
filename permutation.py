06bebc237dd7d899a6fc76044b73f054f8e1fb74
Muskan Bansal <44235014+muskan18@users.noreply.github.com>
2020-05-31 16:12:28 +0530

    find permutation using inbuilt funcions

---
+++ permutation.py
@@ -0,0 +1,12 @@
+from itertools import permutations 
+  
+def allPermutations(str): 
+       
+     permList = permutations(str) 
+  
+     for perm in list(permList): 
+         print (''.join(perm)) 
+        
+if __name__ == "__main__": 
+    str = 'ABC'
+    allPermutations(str) 
